505ebd52ad2d4075ff15486a575125d7a3233c26
Vicente Marçal <vicente.marcal@gmail.com>
2024-05-18 16:47:32 -0300

    core(vm-exchange-6): Installed and set up Dynaconf to manage environment configurations.

---
+++ api/config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from dynaconf import Dynaconf, Validator
+
+ROOT_FOLDER = Path(__file__).parent
+
+settings = Dynaconf(
+    envvar_prefix='vm_exchange',
+    settings_files=[
+        ROOT_FOLDER / 'settings.toml',
+        ROOT_FOLDER / '.secrets.toml'
+    ],
+    default_env='development',  # set the development as a default environment
+    environments=['development', 'testing', 'production'],
+    env_switcher='vm_exchange_env',
+    merge_enabled=True,
+    load_dotenv=False
+)
+
+# Ensures API_KEY exists in .secrets.toml or as an environment variable
+settings.validators.register(Validator("security.API_KEY", must_exist=True, is_type_of=str))
+settings.validators.validate()
