a559899118513203a119a4c84d3afdfabce09fc7
Vicente Marçal <vicente.marcal@gmail.com>
2024-05-19 11:32:32 -0300

    Merge pull request #12
    * feat(vm-exchange-11): Improved endpoints to add ETH currency.

--- api/api_health/endpoints.py
+++ api/api_health/endpoints.py
@@ -7,4 +7,11 @@ router = APIRouter()
 
 @router.get('/health', summary='Check health status of this service API')
 async def service_health() -> ResponseHealthStatus:
+    """
+    **Check the health status of this service API**.\n
+    This function is a FastAPI endpoint that handles a GET request to the '/health' route.
+    It returns a ResponseHealthStatus object with the status set to "health".\n
+    **Returns**:\n
+        - ResponseHealthStatus: An object representing the health status of the service API.
+    """
     return ResponseHealthStatus(status="health")
